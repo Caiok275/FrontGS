92e137795eef3024310fa6865b49c56d7ed25bd7
Caiok275 <caio.kenzo.t@gmail.com>
2025-11-11 22:52:21 -0300

    Commit

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -4,7 +4,17 @@ export default function Home() {
 
     return (
         <div className="w-full h-screen bg-cover bg-center flex flex-col justify-center items-center" style={{ backgroundImage: `url(${bg})` }}>
-        <h1 className="text-6xl text-white font-bold mb-6">FrontGS</h1>
+            <h1 className="text-6xl text-white font-bold mb-6">FrontGS</h1>
+
+            <div className="flex flex-col justify-center items-center text-center text-4xl text-white">
+                <p className="mb-4">Sua plataforma de gerenciamento de serviços</p>
+                <p className="max-w-2xl text-lg">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, aut. Saepe
+                    aliquid quia nulla recusandae ut, et quam voluptates minima voluptatibus
+                    necessitatibus eaque est quidem sunt. Iure, iste non. Nam.
+                </p>
+            </div>
+
         </div>
     );
 }
